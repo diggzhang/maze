42eaa73f6ff8ba606ce4f14f010c944bedccf2a2
diggzhang <diggzhang@gmail.com>
2016-03-04 18:13:49 +0800

    modify(detail.html index.html url.py)
    1. detail页加入option︿(￣︶￣)︿
    2. 解除hardcoded
    3. add namespace

--- maze/polls/urls.py
+++ maze/polls/urls.py
@@ -2,11 +2,13 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
 
     # ex: /polls/5/
+    # url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
 
     # ex: /polls/5/results/
